b0967c1e92c4c5615270d719532976c72781e1f4
derek <dfodker@gmail.com>
2022-03-29 23:24:49 -0400

    Payment label changed

--- credia_app/lib/main.dart
+++ credia_app/lib/main.dart
@@ -222,7 +222,7 @@ class Page2 extends StatelessWidget {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.attach_money_rounded),
-            label: 'School',
+            label: 'Payments',
           ),
         ],
       ),
